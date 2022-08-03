8aac3d26aa353c3b656a22f47cc1ca716515a448
Sasha Bogicevic <sasa.bogicevic@pm.me>
2022-08-03 13:03:52 +0200

    Add functions to handle hydra closing

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,7 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import MyLib
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  MyLib.testClose
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,38 @@
-module MyLib (someFunc) where
+module MyLib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+data HydraState
+  = Initialising
+  | Open
+  | Closed
+  deriving (Eq, Show)
+
+data Utxo = Utxo deriving (Eq, Show)
+
+data HydraHead =
+  HydraHead
+    { hydraHeadState :: HydraState
+    }
+  deriving (Eq, Show)
+
+canInitialise :: HydraState -> Bool
+canInitialise Initialising = True
+canInitialise _ = False
+
+canClose :: HydraState -> Bool
+canClose Open = True
+canClose _ = False
+
+doCloseHydraHead :: HydraState -> HydraState
+doCloseHydraHead s =
+  case s of
+    Open -> Closed
+    x  -> error $ "Cannot close hydra head using state " <> show x
+
+
+testClose :: IO ()
+testClose = do
+  let s = Open
+  if canClose s
+    then print $ doCloseHydraHead s
+    else error "Cannot close hydra head"
